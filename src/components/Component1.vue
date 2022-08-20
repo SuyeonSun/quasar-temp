d9b82002e46ae022fe0a1332d4fce0546484323a
Suyeon Sun <sysun@autocrypt.io>
2022-08-20 14:43:11 +0900

    QMenu API practice

--- src/components/Component1.vue
+++ src/components/Component1.vue
@@ -1,4 +1,54 @@
-<template></template>
+<template>
+  <div class="q-pa-md">
+    <div class="q-gutter-md">
+      <div>1. Basics</div>
+      <q-btn color="primary" label="Basic Menu">
+        <q-menu>
+          <q-list>
+            <q-item clickable v-close-popup>
+              <q-item-section>New Tab</q-item-section>
+            </q-item>
+            <q-item clickable v-close-popup>
+              <q-item-section>New Tab</q-item-section>
+            </q-item>
+            <q-separator />
+            <q-item clickable v-close-popup>
+              <q-item-section>New Tab</q-item-section>
+            </q-item>
+          </q-list>
+        </q-menu>
+      </q-btn>
+
+      <q-btn color="secondary" label="Auto-close-menu">
+        <!-- auto close: Allows any click/tap in the menu to close it -->
+        <q-menu :auto-close="false">
+          <q-list>
+            <q-item clickable>
+              <q-item-section>New Tab</q-item-section>
+            </q-item>
+          </q-list>
+        </q-menu>
+      </q-btn>
+
+      <!-- touch-position, rounded-borders, cursor-pointer, v-close-popup-->
+      <div style="max-width: 300px" class="bg-amber rounded-borders">
+        <div class="fit flex flex-center text-center non-selectable">
+          Click me
+        </div>
+        <q-menu touch-position>
+          <q-list>
+            <q-item clickable v-close-popup>
+              <q-item-section>Branches</q-item-section>
+            </q-item>
+            <q-item>
+              <q-item-section>Branches</q-item-section>
+            </q-item>
+          </q-list>
+        </q-menu>
+      </div>
+    </div>
+  </div>
+</template>
 
 <script setup lang="js"></script>
 
--- src/components/Component2.vue
+++ src/components/Component2.vue
@@ -1,4 +1,47 @@
-<template></template>
+<template>
+  <div class="q-pa-md q-gutter-md">
+    <div>2. Submenus (side, anchor)</div>
+    <q-btn color="primary" label="Click me" style="width: 280px">
+      <q-menu fit>
+        <q-list dense>
+          <q-item clickable v-close-popup>
+            <q-item-section>Menu1</q-item-section>
+          </q-item>
+          <q-item clickable v-close-popup>
+            <q-item-section>Menu2</q-item-section>
+          </q-item>
+          <q-separator />
+
+          <q-item clickable>
+            <q-item-section>Menu3</q-item-section>
+            <q-item-section side>
+              <q-icon name="keyboard_arrow_right" />
+            </q-item-section>
+
+            <q-menu self="top start" anchor="top end">
+              <q-list>
+                <q-item v-for="n in 3" :key="n" dense clickable>
+                  <q-item-section> Submenu </q-item-section>
+                  <q-item-section side>
+                    <q-icon name="keyboard_arrow_right" />
+                  </q-item-section>
+                  <!-- anchor self-->
+                  <q-menu anchor="top end" self="top start">
+                    <q-list>
+                      <q-item v-for="n in 3" :key="n" dense clickable>
+                        <q-item-section>3rd level item</q-item-section>
+                      </q-item>
+                    </q-list>
+                  </q-menu>
+                </q-item>
+              </q-list>
+            </q-menu>
+          </q-item>
+        </q-list>
+      </q-menu>
+    </q-btn>
+  </div>
+</template>
 
 <script setup lang="js"></script>
 
