bd4f0ff419141e8346ee46f852997870ea11e195
Suyeon Sun <sysun@autocrypt.io>
2022-08-20 19:24:04 +0900

    vue, css, js로 dropdown 메뉴 구현

--- src/components/Component4.vue
+++ src/components/Component4.vue
@@ -1,5 +1,82 @@
-<template></template>
+<template>
+  <div class="q-pa-md q-gutter-md">
+    <div>4. Clickable dropdown using vue</div>
+    <div class="drop-box">
+      <button @click="toggleMenu" class="drop-btn">Dropdown</button>
+      <div v-if="isMenuOpen" class="drop-content">
+        <a
+          @click="toggleMenu"
+          :href="`${element.link}`"
+          v-for="element in menu"
+          :key="element"
+          >{{ element.label }}</a
+        >
+      </div>
+    </div>
+  </div>
+</template>
 
-<script setup lang="js"></script>
+<script setup>
+import { ref } from "vue";
 
-<style scoped></style>
+const isMenuOpen = ref(false);
+const toggleMenu = () => {
+  isMenuOpen.value = !isMenuOpen.value;
+};
+
+const menu = [
+  { link: "https://google.com", label: "Google" },
+  { link: "#", label: "nothing" },
+];
+</script>
+
+<style scoped lang="scss">
+.drop-btn {
+  background-color: #3498db;
+  color: white;
+  padding: 16px;
+  font-size: 16px;
+  border: none;
+  cursor: pointer;
+}
+
+//.drop-btn:hover,
+//.drop-btn:focus {
+//  background-color: #2980b9;
+//}
+
+.drop-btn {
+  &:hover,
+  &:focus {
+    background-color: #2980b9;
+  }
+}
+
+// check
+.drop-box {
+  position: relative;
+  display: inline-block;
+}
+
+// check
+.drop-content {
+  //display: none;
+  position: absolute;
+  background-color: #f1f1f1;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+  z-index: 1;
+}
+
+.drop-content {
+  a {
+    color: black;
+    padding: 12px 16px;
+    text-decoration: none;
+    display: block;
+    &:hover {
+      background-color: #ddd;
+    }
+  }
+}
+</style>
--- src/components/Component5.vue
+++ src/components/Component5.vue
@@ -1,5 +1,93 @@
-<template></template>
+<template>
+  <div class="drop-down">
+    <div>5. Clickable dropdown using css, js</div>
+    <button @click="toggleMenu()" class="button">Drop</button>
+    <div style="display: none" id="drop-content">
+      <a hreaf="#">Menu1</a>
+      <a hreaf="#">Menu2</a>
+      <a hreaf="#">Menu3</a>
+      <a hreaf="#">Menu4</a>
+      <a hreaf="#">Menu5</a>
+    </div>
 
-<script setup lang="js"></script>
+    <div class="q-pt-lg">5-1. display: none과 visibility: hidden의 차이</div>
+    <div id="a" class="box">a</div>
+    <div id="b" class="box">b</div>
+    <div id="c" class="box">c</div>
 
-<style scoped></style>
+    <div class="q-pt-lg">5-2. position 속성</div>
+    <div id="static1">1. static</div>
+    <div id="static2">2. relative</div>
+    <div id="static3-parent">
+      <div id="static3">
+        3. absolute: static을 제외한 가장 가까운 상위 요소를 기준으로 배치
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const toggleMenu = () => {
+  let click = document.getElementById("drop-content");
+  if (click.style.display === "none") {
+    click.style.display = "block";
+  } else {
+    click.style.display = "none";
+  }
+};
+</script>
+
+<style scoped>
+.box {
+  width: 100px;
+  height: 100px;
+  /*display: inline;*/
+  /*display: none;*/
+  /*display: block;*/
+  display: inline-block;
+}
+
+#a {
+  background-color: yellow;
+  visibility: hidden;
+}
+
+#b {
+  background-color: #2f9d27;
+  display: none;
+}
+
+#c {
+  background-color: #f29661;
+}
+
+#static1 {
+  width: 100px;
+  height: 100px;
+  background-color: green;
+  position: static;
+}
+
+#static2 {
+  width: 100px;
+  height: 100px;
+  background-color: yellow;
+  position: relative;
+  left: 100px;
+}
+
+#static3 {
+  width: 100px;
+  height: 100px;
+  background-color: red;
+  position: absolute;
+  left: 50px;
+}
+
+#static3-parent {
+  width: 100px;
+  height: 100px;
+  background-color: black;
+  position: relative;
+}
+</style>
