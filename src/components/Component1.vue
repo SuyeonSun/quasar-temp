eb39ec6f64c355fadc1cfa4a014fbc7df413c4a6
Suyeon Sun <sysun@autocrypt.io>
2022-08-23 16:48:01 +0900

    vee-validate 설치

--- src/components/Component1.vue
+++ src/components/Component1.vue
@@ -1,4 +1,11 @@
-<template></template>
+<template>
+  <Form v-slot="{ values }">
+    <Field name="selectBox1" v-slot="{ value, field }">
+      <q-select outlined model-value="value" v-bind="field" label="Outlined" />
+    </Field>
+    <Field name="selectBox2" v-slot="{ value, field }"> </Field>
+  </Form>
+</template>
 
 <script setup></script>
 
