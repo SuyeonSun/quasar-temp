a4ecdab8e3a8111fb9d642252462a473514e8559
SuyeonSun <suninisy@gmail.com>
2022-09-05 23:03:11 +0900

    select box 연습

--- src/components/Component4.vue
+++ src/components/Component4.vue
@@ -1,5 +1,31 @@
-<template></template>
+<template>
+  <div class="text-orange">7. html select</div>
+  <select name="cars" id="cars">
+    <option value="volvo">Volvo</option>
+    <option value="saab">Saab</option>
+    <option value="mercedes">Mercedes</option>
+    <option value="audi">Audi</option>
+  </select>
 
-<script setup lang="js"></script>
+  <form>
+    <label for="cars">Choose a car:</label>
+    <select name="cars" id="cars">
+      <optgroup label="Swedish Cars">
+        <option value="volvo">Volvo</option>
+        <option value="saab">Saab</option>
+      </optgroup>
+      <optgroup label="German Cars">
+        <option value="mercedes">Mercedes</option>
+        <option value="audi">Audi</option>
+      </optgroup>
+    </select>
+  </form>
+
+  <!--  <select size="6" style="overflow: scroll">-->
+  <!--    <option v-for="o in options">{{ o }}</option>-->
+  <!--  </select>-->
+</template>
+
+<script setup></script>
 
 <style scoped></style>
