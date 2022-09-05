fdba9edf2ad29fd9302455a687aa16d3ef81a4af
SuyeonSun <suninisy@gmail.com>
2022-09-06 00:00:48 +0900

    css position 연습

--- src/components/Component3.vue
+++ src/components/Component3.vue
@@ -1,5 +1,38 @@
-<template></template>
+<template>
+  <div class="dashed-box">
+    Dashed box
+    <span class="gold-box">Gold box</span>
+    <span class="green-box">Green box</span>
+  </div>
+</template>
 
-<script setup lang="js"></script>
+<script setup></script>
 
-<style scoped></style>
+<style scoped>
+.dashed-box {
+  position: relative;
+  border: dashed;
+  height: 8em;
+  margin-bottom: 1em;
+  margin-top: 2em;
+}
+
+.gold-box {
+  position: absolute;
+  background-color: gold;
+  width: 80%;
+  left: 60px;
+  top: 3em;
+}
+
+.green-box {
+  position: absolute;
+  z-index: 1; /* put .green-box above .dashed-box */
+  background: lightgreen;
+  width: 20%;
+  left: 65%;
+  top: -25px;
+  height: 7em;
+  opacity: 0.9;
+}
+</style>
--- src/components/Component4.vue
+++ src/components/Component4.vue
@@ -1,5 +1,41 @@
-<template></template>
+<template>
+  <div class="parent">
+    <div class="relative">relative</div>
+    <div class="fixed">fixed</div>
+    <div class="absolute">absolute</div>
+  </div>
+</template>
 
-<script setup lang="js"></script>
+<script setup></script>
 
-<style scoped></style>
+<style scoped>
+.parent {
+  background-color: #31ccec;
+  width: 300px;
+  height: 300px;
+}
+
+div.relative {
+  position: relative;
+  left: 30px;
+  width: 100px;
+  height: 100px;
+  background-color: red;
+}
+
+div.fixed {
+  position: fixed;
+  bottom: 0;
+  right: 0;
+  width: 100px;
+  height: 100px;
+  background-color: blue;
+}
+
+div.absolute {
+  position: absolute;
+  width: 100px;
+  height: 100px;
+  background-color: yellow;
+}
+</style>
