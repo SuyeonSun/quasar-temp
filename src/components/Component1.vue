84e81aa533b220032c9ee8ed711a9fbcad3eadde
Suyeon Sun <sysun@autocrypt.io>
2022-09-23 15:50:35 +0900

    quasar clip board event 구현

--- src/components/Component1.vue
+++ src/components/Component1.vue
@@ -1,12 +1,23 @@
-<template>
-  <Form v-slot="{ values }">
-    <Field name="selectBox1" v-slot="{ value, field }">
-      <q-select outlined model-value="value" v-bind="field" label="Outlined" />
-    </Field>
-    <Field name="selectBox2" v-slot="{ value, field }"> </Field>
-  </Form>
-</template>
+<script setup>
+import { copyToClipboard } from "quasar";
 
-<script setup></script>
+const copyLink = () => {
+  copyToClipboard("https://www.google.com/")
+    .then(() => {
+      console.log("===== success");
+      // create Notify
+    })
+    .catch(() => {
+      console.log("===== error");
+      // create Notify
+    });
+};
+</script>
+
+<template>
+  <div>
+    <q-btn color="black" label="Copy Link" @click="copyLink" />
+  </div>
+</template>
 
 <style scoped></style>
