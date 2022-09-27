726425ae9eee8a467c7c877a17214828c701e796
Suyeon Sun <sysun@autocrypt.io>
2022-09-27 16:31:28 +0900

    검색 바 구현

--- src/components/Component1.vue
+++ src/components/Component1.vue
@@ -1,12 +1,62 @@
 <template>
-  <Form v-slot="{ values }">
-    <Field name="selectBox1" v-slot="{ value, field }">
-      <q-select outlined model-value="value" v-bind="field" label="Outlined" />
-    </Field>
-    <Field name="selectBox2" v-slot="{ value, field }"> </Field>
-  </Form>
+  <div class="q-pa-md">
+    <q-input
+      rounded
+      outlined
+      v-model="searchValue"
+      type="text"
+      placeholder="Search for the EV you'd like to know!"
+      @keyup="updateSearchResult"
+      style="width: 300px"
+    >
+      <template v-slot:append>
+        <q-icon name="search" @click="handleClick" />
+      </template>
+    </q-input>
+    <!--    <button @click="handleClick">검색</button>-->
+    <ul>
+      <!-- 검색 결과 -->
+      <li>{{ searchResult }}</li>
+    </ul>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref, watch } from "vue";
+
+const searchValue = ref("");
+const dataInServer = ["a", "ab", "abc", "d", "e", "f", "g"];
+const searchResult = ref("");
+
+watch(
+  () => searchResult.value,
+  (value) => {
+    searchResult.value = value;
+  }
+);
+
+const searchServer = () => {
+  // 실제 구현 시, 서버에 searchValue 값 요청해서 결과 값 스토어 저장
+  if (searchValue.value === "") {
+    searchResult.value = "";
+  }
+  dataInServer.forEach((v) => {
+    if (v === searchValue.value) {
+      searchResult.value = v;
+      console.log("=====", v);
+    }
+  });
+};
+
+// 0.7초 후 입력 값이 없으면, 서버에 요청
+const updateSearchResult = () => {
+  setTimeout(searchServer, 700);
+};
+
+// 검색 버튼 클릭 시, 서버에 요청
+const handleClick = () => {
+  searchServer();
+};
+</script>
 
 <style scoped></style>
--- src/components/Component2.vue
+++ src/components/Component2.vue
@@ -1,5 +1,34 @@
-<template></template>
+<template>
+  <div class="q-pa-md">
+    <input
+      type="text"
+      placeholder="Search for the EV you'd like to know!"
+      @input="updateSearchResult"
+    />
+    <ul v-if="searchResult.length > 0">
+      <li>{{ searchResult }}</li>
+    </ul>
+  </div>
+</template>
 
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+const dataInServer = ["a", "b", "c", "d", "e", "f", "g"];
+const searchResult = ref("");
+
+const updateSearchResult = (e) => {
+  if (e.target.value === "") {
+    searchResult.value = "";
+  }
+  dataInServer.forEach((v) => {
+    // 서버로 e.target.value 전송 후 받은 response 값 searchResult에 저장
+    if (v === e.target.value) {
+      searchResult.value = v;
+      console.log("=====", v);
+    }
+  });
+};
+</script>
 
 <style scoped></style>
