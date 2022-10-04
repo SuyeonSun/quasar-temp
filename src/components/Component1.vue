67e5d6a6d289666dab0d828f1ae871f6dafb7080
Suyeon Sun <sysun@autocrypt.io>
2022-10-04 14:19:40 +0900

    create notify

--- src/components/Component1.vue
+++ src/components/Component1.vue
@@ -1,11 +1,22 @@
+<template>
+  <div>
+    <q-btn color="black" label="Copy Link" @click="copyLink" />
+  </div>
+</template>
+
 <script setup>
-import { copyToClipboard } from "quasar";
+import { copyToClipboard, Notify } from "quasar";
 
 const copyLink = () => {
   copyToClipboard("https://www.google.com/")
     .then(() => {
       console.log("===== success");
       // create Notify
+      Notify.create({
+        message: "Copied to clipboard",
+        color: "grey",
+        position: "top",
+      });
     })
     .catch(() => {
       console.log("===== error");
@@ -13,11 +24,3 @@ const copyLink = () => {
     });
 };
 </script>
-
-<template>
-  <div>
-    <q-btn color="black" label="Copy Link" @click="copyLink" />
-  </div>
-</template>
-
-<style scoped></style>
