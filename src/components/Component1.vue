1c9f782a6c5d578aa5e03f46cc10d769419ffa77
Suyeon Sun <sysun@autocrypt.io>
2022-10-04 15:06:24 +0900

    LinkedIn ShareNetwork 추가

--- src/components/Component1.vue
+++ src/components/Component1.vue
@@ -1,28 +1,32 @@
 <template>
   <!-- facebook / LinkedIn / Twitter / WhatsApp-->
-  <q-btn color="black">
-    <ShareNetwork
-      network="WhatsApp"
-      url="https://lindaojo.com/blog/awesome-article"
-      title="Awesome Article"
-      description="This is an awesome article for awesome readers"
-      hashtags="Frontend, Programming"
-    >
-      <span>WhatsApp</span>
-    </ShareNetwork>
+  <!--  <q-btn color="black">-->
+  <!--    <ShareNetwork-->
+  <!--      network="WhatsApp"-->
+  <!--      url="https://lindaojo.com/blog/awesome-article"-->
+  <!--      title="Awesome Article"-->
+  <!--      description="This is an awesome article for awesome readers"-->
+  <!--      hashtags="Frontend, Programming"-->
+  <!--    >-->
+  <!--      <span>WhatsApp</span>-->
+  <!--    </ShareNetwork>-->
+  <!--  </q-btn>-->
 
-    <!--    <ShareNetwork-->
-    <!--        network="facebook"-->
-    <!--        url="https://lindaojo.com/blog/awesome-article"-->
-    <!--        title="Awesome Article"-->
-    <!--        description="This is an awesome article for awesome readers"-->
-    <!--        hashtags="Frontend, Programming"-->
-    <!--    >-->
-    <!--      <q-btn color="black" label="facebook" no-caps />-->
-    <!--    </ShareNetwork>-->
-  </q-btn>
+  <ShareNetwork
+    network="facebook"
+    title="titleName"
+    description="description"
+    url="https://www.autocrypt.co.kr/"
+  >
+    <q-btn color="black" label="facebook" no-caps />
+  </ShareNetwork>
+
+  <ShareNetwork
+    network="LinkedIn"
+    title="titleName"
+    description="description"
+    url="https://www.autocrypt.co.kr/"
+  >
+    <q-btn color="black" label="LinkedIn" no-caps />
+  </ShareNetwork>
 </template>
-
-<script setup></script>
-
-<style scoped></style>
