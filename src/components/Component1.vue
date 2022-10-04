7a54f28fd734ab7b4ce9b77c14684920499b8e3c
Suyeon Sun <sysun@autocrypt.io>
2022-10-04 12:31:22 +0900

    debounce 구현

--- src/components/Component1.vue
+++ src/components/Component1.vue
@@ -1,12 +1,33 @@
 <template>
-  <Form v-slot="{ values }">
-    <Field name="selectBox1" v-slot="{ value, field }">
-      <q-select outlined model-value="value" v-bind="field" label="Outlined" />
-    </Field>
-    <Field name="selectBox2" v-slot="{ value, field }"> </Field>
-  </Form>
+  <!--  <q-input-->
+  <!--    :model-value="debounceValue"-->
+  <!--    @keyup="debounceMethod"-->
+  <!--    debounce="700"-->
+  <!--    rounded-->
+  <!--    outlined-->
+  <!--    dense-->
+  <!--  />-->
+  <q-input
+    :model-value="debounceValue"
+    @update:model-value="debounceMethod"
+    debounce="700"
+    rounded
+    outlined
+    dense
+  />
 </template>
 
-<script setup></script>
+<script setup>
+// yarn loadash 설치하여 debounce 적용
+// 방법 2. quasar debounce 적용 // import {debounce} from "quasar";
+import { debounce } from "lodash";
+const debounceValue = undefined;
 
-<style scoped></style>
+// const debounceMethod = debounce((e) => {
+//   console.log("send this input value to server", e.target.value);
+// }, 700);
+
+const debounceMethod = (value) => {
+  console.log("send this input value to server", value);
+};
+</script>
