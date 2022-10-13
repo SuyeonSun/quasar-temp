99154e6be750673c19ab3d24a27b86fba114e591
Suyeon Sun <sysun@autocrypt.io>
2022-10-13 12:33:29 +0900

    eviqpick10 initial commit

---
+++ src/components/EviqPick10Chart.vue
@@ -0,0 +1,62 @@
+<template>
+  <Bar
+    :chart-options="chartOptions"
+    :chart-data="chartData"
+    chart-id="bar-chart"
+    dataset-id-key="label"
+    :width="400"
+    :height="50"
+  />
+</template>
+
+<script setup>
+import { Bar } from "vue-chartjs";
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+} from "chart.js";
+
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+);
+const labels = "";
+const chartData = {
+  labels: [labels], // TODO: labels 제거
+  datasets: [{ data: [100], backgroundColor: "#00d392" }],
+};
+
+const chartOptions = {
+  indexAxis: "y",
+  responsive: true,
+  scales: {
+    x: {
+      grid: {
+        display: false,
+      },
+    },
+    y: {
+      grid: {
+        display: false,
+      },
+    },
+  },
+  plugins: {
+    legend: {
+      display: false,
+    },
+    tooltip: {
+      enabled: false,
+    },
+  },
+};
+</script>
