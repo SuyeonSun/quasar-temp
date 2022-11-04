084d5e56de59dbd497a3b1251f16eb33f9b392e8
Suyeon Sun <sysun@autocrypt.io>
2022-11-04 17:54:32 +0900

    one page scroll

---
+++ src/components/svgTest.vue
@@ -0,0 +1,16 @@
+<script setup>
+import * as d3 from "d3";
+
+const svg = d3
+  .select(".chart")
+  .append("svg")
+  .attr("width", 534)
+  .attr("height", 400);
+
+// svg.selectAll("*").remove();
+</script>
+
+<template>
+  ??
+  <div class="chart"></div>
+</template>
