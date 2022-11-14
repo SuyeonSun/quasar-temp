1dec221850c32b4030fb20e26d8d76e9e9699f6b
Suyeon Sun <sysun@autocrypt.io>
2022-11-14 23:46:36 +0900

    practice other css

--- src/components/Component1.vue
+++ src/components/Component1.vue
@@ -1,5 +1,6 @@
 <template>
-  <div></div>
+  <div style="width: 100px; height: 100px; background-color: pink" class="rotate-45"></div>
+  <div style="width: 100px; height: 100px; background-color: orange" class="rotate-90"></div>
 </template>
 
 <script setup></script>
