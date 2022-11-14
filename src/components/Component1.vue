32833008d9f15e18ea7a7ea5ef96addeae487d6d
Suyeon Sun <sysun@autocrypt.io>
2022-11-14 15:20:02 +0900

    main 수정

--- src/components/Component1.vue
+++ src/components/Component1.vue
@@ -1,10 +1,5 @@
 <template>
-  <Form v-slot="{ values }">
-    <Field name="selectBox1" v-slot="{ value, field }">
-      <q-select outlined model-value="value" v-bind="field" label="Outlined" />
-    </Field>
-    <Field name="selectBox2" v-slot="{ value, field }"> </Field>
-  </Form>
+  <div></div>
 </template>
 
 <script setup></script>
