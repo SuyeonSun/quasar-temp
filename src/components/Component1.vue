ccd3072ce16c709acf0a9c95249f138278c85a2c
Suyeon Sun <sysun@autocrypt.io>
2022-11-14 21:20:37 +0900

    dark mode

--- src/components/Component1.vue
+++ src/components/Component1.vue
@@ -1,7 +1,22 @@
 <template>
-  <div></div>
+  <q-btn @click="handleDarkMode">
+    다크모드
+  </q-btn>
 </template>
 
-<script setup></script>
+<script setup>
+import {useQuasar, Dark} from 'quasar'
+import {onMounted} from "vue";
+
+const $q = useQuasar()
+// onMounted(() => {
+//   console.log('===========', $q.dark.isActive)
+// })
+
+const handleDarkMode = () => {
+  // $q.dark.set(!$q.dark.isActive)
+  Dark.toggle()
+}
+</script>
 
 <style scoped></style>
